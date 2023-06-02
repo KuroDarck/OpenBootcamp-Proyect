dc9d84ec01cc22e28efdd703a99309579fbb966b
KuroBlack <blackjiackherrera@gmail.com>
2023-06-01 21:51:37 -0500

    Tarea  3 Terminada

--- Introduccion a la programacion/src/Ejercicios_tema_3.java
+++ Introduccion a la programacion/src/Ejercicios_tema_3.java
@@ -2,16 +2,16 @@ public class Ejercicios_tema_3 {
 
     public static void main(String[] args) {
 
-        //Crear una función con tres parámetros que sean números que se suman entre sí.
+        //Llamar a la función en el main y darle valores.
         System.out.println("El resultado entero es: "+Sumar(10,20,30));
 
         System.out.println("El resultado double es: "+Sumar(10.5,20.3,3.0));
 
         //Crear un objeto miCoche en el main y añadirle una puerta.
         coche miCoche=new coche();
-        miCoche.incremetarPuertas(1);
-        
-        //Crear un objeto miCoche en el main y añadirle una puerta.
+        miCoche.incremetarPuertas();
+
+        //Mostrar el número de puertas que tiene el objeto.
         System.out.println("El coche tiene "+miCoche.getPuertas() +" Puertas" );
     }
 
@@ -31,9 +31,9 @@ class coche{
     //Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
     private int puertas=3;
 
-    //Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
-    public void incremetarPuertas(int puertas){
-        this.puertas=(this.puertas+puertas);
+    //Una función que incremente el número de puertas que tiene el coche.
+    public void incremetarPuertas(){
+        this.puertas++;
     }
 
     public int getPuertas() {
