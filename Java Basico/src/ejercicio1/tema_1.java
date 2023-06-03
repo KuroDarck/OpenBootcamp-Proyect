17ac82a3667696bd87cc00d88089ed7cf4888c44
KuroBlack <blackjiackherrera@gmail.com>
2023-06-02 20:53:25 -0500

    tarea 1 Terminada

---
+++ Java Basico/src/ejercicio1/tema_1.java
@@ -0,0 +1,19 @@
+package ejercicio1;
+
+public class tema_1 {
+
+    public static void main(String[] args) {
+
+        int numeroEntero=10;
+        long numeroLargo=1000000000L;
+        double numeroDecimal=3.14;
+        boolean valorBooleano=true;
+        String texto="Hola, mundo!";
+
+        System.out.println("Este es un  Valor : "+numeroEntero+" Entero\n");
+        System.out.println("Este es un  Valor : "+numeroLargo+" Long\n");
+        System.out.println("Este es un  Valor : "+numeroDecimal+" Decimal\n");
+        System.out.println("Este es un  Valor : "+texto+" String\n");
+    }
+
+}
